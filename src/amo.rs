425caf3365de820d01753a16182b02a7527c01a7
sgdxbc <guangda@u.nus.edu>
2024-10-27 14:04:30 +0800

    Iterate design for at most once

--- src/amo.rs
+++ src/amo.rs
@@ -1,10 +1,13 @@
-use crate::{join, publish, serve, subscribe, Addr, App, Handle, Id, Invoke, Op, Res, Source};
+use std::collections::BTreeMap;
+
+use crate::{Addr, App, Id, Op, Res};
 
 pub mod messages {
-    use crate::{Addr, Op, Res};
+    use crate::{Addr, Id, Op, Res};
 
     #[derive(Debug, Clone)]
     pub struct Request {
+        pub id: Id,
         pub seq: u32,
         pub op: Op,
         pub client_addr: Addr,
@@ -17,76 +20,230 @@ pub mod messages {
     }
 }
 
-pub async fn client_task(id: Id, addr: Addr) {
-    subscribe::<Addr, messages::Reply, _>(move |source| {
-        struct ServeTask {
-            seq: u32,
-            source: Source<messages::Reply>,
-            id: Id,
-            addr: Addr,
-        }
-        impl Handle<Op, Res> for ServeTask {
-            async fn handle(&mut self, request: Op) -> Res {
-                self.seq += 1;
-                let request = messages::Request {
-                    seq: self.seq,
-                    op: request,
-                    client_addr: self.addr,
-                };
-                publish(self.id, request);
-                while let Some(reply) = self.source.recv().await {
-                    if reply.seq != self.seq {
-                        continue;
-                    }
-                    return reply.result;
-                }
-                unreachable!()
-            }
-        }
-        serve::<Invoke, Op, _>(ServeTask {
-            seq: 0,
-            source,
-            id,
-            addr,
-        })
-    })
-    .await;
+pub trait ClientContext {
+    fn finalize_invoke(&mut self, result: Res);
+    fn send_to_server(&mut self, request: messages::Request);
+    // TODO resend timeout
 }
 
-pub async fn server_task(app: App) {
-    struct Execute;
+pub trait ServerContext {
+    fn send(&mut self, addr: Addr, reply: messages::Reply);
+}
 
-    let reply_tasks = subscribe::<Id, messages::Request, _>(|mut source| {
-        let mut saved_reply = Option::<messages::Reply>::None;
-        async move {
-            while let Some(request) = source.recv().await {
-                if let Some(reply) = &saved_reply {
-                    if request.seq < reply.seq {
-                        continue;
-                    }
-                    if request.seq == reply.seq {
-                        publish(request.client_addr, reply.clone());
-                    }
-                }
-                let result = crate::request(Execute, request.op).await;
-                let reply = messages::Reply {
-                    seq: request.seq,
-                    result,
-                };
-                saved_reply = Some(reply.clone());
-                publish(request.client_addr, reply)
-            }
+mod typed {
+    use crate::{Addr, App, Id, Op, Res};
+
+    use super::messages;
+
+    #[derive(Debug, Clone)]
+    pub struct Replied(u32);
+
+    #[derive(Debug, Clone)]
+    pub struct WaitingReply(u32, pub Op);
+
+    impl Replied {
+        pub fn new() -> Self {
+            Self(0)
         }
-    });
 
-    struct ExecuteTask(App);
-    impl Handle<Op, Res> for ExecuteTask {
-        async fn handle(&mut self, request: Op) -> Res {
-            let Self(app) = self;
-            app.execute(request)
+        // transition makes both constraints of
+        // * what state can move to (similar to cannot directly Clean->Clean, must go through
+        //   Clean->Dirty->InFlight->Clean)
+        // * what side effect can/has to be made during transition (similar to InFlight->Clean must
+        //   go through a blocking hardware-synchronized `fence()`)
+        //   in the ideal cases this builds a bijection between transition and side effects
+        //   + if a Replied -> WaitingReply, a `send_to_server(...)` must be issued
+        //   + `send_to_server(...)` is only issued in Replied->WaitingReply and
+        //     WaitingReply->WaitingReply transitions
+        //   so both direction of reasoning can be benefitted
+        pub fn request(
+            self,
+            id: Id,
+            op: Op,
+            client_addr: Addr,
+            context: &mut impl super::ClientContext,
+        ) -> WaitingReply {
+            let seq = self.0 + 1;
+            let request = messages::Request {
+                id,
+                seq,
+                op: op.clone(),
+                client_addr,
+            };
+            context.send_to_server(request);
+            // TODO set timer
+            WaitingReply(seq, op)
         }
     }
-    let execute_task = serve::<Execute, Op, _>(ExecuteTask(app));
 
-    join(reply_tasks, execute_task).await;
+    impl WaitingReply {
+        pub fn replied(self, result: Res, context: &mut impl super::ClientContext) -> Replied {
+            context.finalize_invoke(result);
+            // TODO if necessary unset timer here
+            Replied(self.0)
+        }
+
+        pub fn timeout(
+            self,
+            id: Id,
+            client_addr: Addr,
+            context: &mut impl super::ClientContext,
+        ) -> Self {
+            let Self(seq, op) = self;
+            let request = messages::Request {
+                id,
+                seq,
+                op: op.clone(),
+                client_addr,
+            };
+            context.send_to_server(request);
+            // TODO if necessary reset timer here
+            Self(seq, op)
+        }
+
+        // utilities
+        pub fn is_replied(&self, seq: u32) -> bool {
+            seq == self.0
+        }
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Replying(u32, pub Res);
+
+    impl Replied {
+        pub fn execute(
+            self,
+            seq: u32,
+            op: Op,
+            addr: Addr,
+            app: &mut App,
+            context: &mut impl super::ServerContext,
+        ) -> Replying {
+            // alternative: check for seq > self.0 i.e. the transition is permitted by semantic
+            // this "inline checking" style probably won't work for even slightly more involved
+            // cases, so currently taking this "just transition" style and rely on protocol side
+            // checking
+            let result = app.execute(op);
+            let reply = messages::Reply {
+                seq,
+                result: result.clone(),
+            };
+            context.send(addr, reply);
+            Replying(seq, result)
+        }
+    }
+
+    impl Replying {
+        pub fn received(self) -> Replied {
+            Replied(self.0)
+        }
+
+        pub fn reply(self, addr: Addr, context: &mut impl super::ServerContext) -> Self {
+            let Self(seq, result) = self;
+            let reply = messages::Reply {
+                seq,
+                result: result.clone(),
+            };
+            context.send(addr, reply);
+            Replying(seq, result)
+        }
+
+        // utilities
+        pub fn is_received(&self, seq: u32) -> bool {
+            seq > self.0
+        }
+
+        pub fn is_replying(&self, seq: u32) -> bool {
+            seq == self.0
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum ClientRequestState {
+    Replied(typed::Replied),
+    WaitingReply(typed::WaitingReply),
+}
+
+#[derive(Debug)]
+pub struct ClientState {
+    id: Id,
+    addr: Addr,
+    request: ClientRequestState,
+}
+
+impl ClientState {
+    pub fn new(id: Id, addr: Addr) -> Self {
+        Self {
+            id,
+            addr,
+            request: ClientRequestState::Replied(typed::Replied::new()),
+        }
+    }
+}
+
+impl ClientState {
+    pub fn invoke(&mut self, op: Op, context: &mut impl ClientContext) {
+        let ClientRequestState::Replied(state) = self.request.clone() else {
+            unimplemented!()
+        };
+        let state = state.request(self.id, op.clone(), self.addr, context);
+        self.request = ClientRequestState::WaitingReply(state)
+    }
+
+    pub fn handle_timeout(&mut self, context: &mut impl ClientContext) {
+        let ClientRequestState::WaitingReply(state) = self.request.clone() else {
+            unimplemented!() // or return, depending on unset/reset style timer API
+        };
+        let state = state.timeout(self.id, self.addr, context);
+        self.request = ClientRequestState::WaitingReply(state)
+    }
+
+    pub fn handle_reply(&mut self, reply: messages::Reply, context: &mut impl ClientContext) {
+        let ClientRequestState::WaitingReply(state) = self.request.clone() else {
+            return;
+        };
+        if state.is_replied(reply.seq) {
+            let state = state.replied(reply.result, context);
+            self.request = ClientRequestState::Replied(state)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ServerState {
+    app: App,
+    replying: BTreeMap<Id, typed::Replying>,
+}
+
+impl ServerState {
+    pub fn new(app: App) -> Self {
+        Self {
+            app,
+            replying: Default::default(),
+        }
+    }
+
+    pub fn handle_request(&mut self, request: messages::Request, context: &mut impl ServerContext) {
+        let state = if let Some(state) = self.replying.get_mut(&request.id) {
+            if state.is_received(request.seq) {
+                state.clone().received()
+            } else {
+                if state.is_replying(request.seq) {
+                    *state = state.clone().reply(request.client_addr, context)
+                }
+                return;
+            }
+        } else {
+            typed::Replied::new()
+        };
+        let state = state.execute(
+            request.seq,
+            request.op,
+            request.client_addr,
+            &mut self.app,
+            context,
+        );
+        self.replying.insert(request.id, state);
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,47 +1,4 @@
-use std::future::Future;
-
 pub mod amo;
-pub mod pb;
-
-pub async fn subscribe<K, M, F>(create_task: impl FnMut(Source<M>) -> F)
-where
-    F: Future<Output = ()>,
-{
-    let _ = create_task;
-}
-
-pub struct Source<M>(M);
-
-impl<M> Source<M> {
-    pub async fn recv(&mut self) -> Option<M> {
-        todo!()
-    }
-}
-
-pub fn publish<K, M>(selector: K, message: M) {
-    let _ = (selector, message);
-}
-
-pub async fn serve<K, Q, P>(handle: impl Handle<Q, P>) {
-    let _ = handle;
-}
-
-pub trait Handle<Q, P> {
-    fn handle(&mut self, request: Q) -> impl Future<Output = P>;
-}
-
-pub async fn request<K, Q, P>(selector: K, message: Q) -> P {
-    let _ = (selector, message);
-    todo!()
-}
-
-pub async fn join<T, U>(
-    task: impl Future<Output = T>,
-    other_task: impl Future<Output = U>,
-) -> (T, U) {
-    let _ = (task, other_task);
-    todo!()
-}
 
 #[derive(Debug, Clone)]
 pub struct Op;
